cfbd715896e464b5a74cf27100f07a6a54d85503
Steve Chen <stevechen@FB-Support-R2s-MacBook-Pro.local>
2020-10-05 23:16:52 -0400

    Initial commit. Adds following incredmental button - Increments number of followers on button click - Watches followers data to output console.log when a follower is gained - mounted() lifecycle method adds 1 immediate follow on mount

---
+++ src/App.vue
@@ -0,0 +1,65 @@
+<template>
+<div id="app">
+  @{{ user.username }} - {{ fullName}}
+  <strong>Followers: </strong> {{ followers }}
+  <button @click="followUser">
+    Follow
+  </button>
+</div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      followers: 0,
+      user: {
+        id: 1,
+        username: 'stevechenDC',
+        firstName: 'Steve',
+        lastName: 'Chen',
+        email: 'stevechenweb@gmail.com',
+        isAdmin: true
+      }
+    }
+  }, 
+  watch: {
+    followers(newFollowerCount, oldFollowerCount) {
+      if (oldFollowerCount < newFollowerCount) {
+        console.log(`${this.user.username} has gained a follower!`)
+      }
+    }
+  },
+  computed: {
+    fullName(){
+      return `${this.user.firstName} ${this.user.lastName}`
+    }
+  },
+  methods: {
+    followUser() {
+      this.followers++
+      // this.followers = this.followers + 1
+      // this.follower += 1
+    }
+  },
+  mounted() {
+    this.followUser();
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  margin-top: 60px;
+}
+</style>
